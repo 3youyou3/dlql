fb8d1dde82a649d0b3ee5d1b915217d3249b66a2
youyou <youyou@youyoudeMacBook-Pro.local>
2014-07-07 23:04:22 +0800

    update quick cocos2d-x to 2.2.4

---
+++ dlql/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+../quick-cocos2d-x-2.2.3-rc/player/mac/quick-x-player.app/Contents/MacOS/quick-x-player -workdir /Users/youyou/Desktop/workspace/project/dlql/dlql -file scripts/main.lua
